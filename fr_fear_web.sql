4d5f559e8b2a198a21658f96e31d97dc8a453192
mahmoud yousef <mahmoudyousef@gmail.com>
2024-04-27 23:09:41 +0200

    update database

--- fr_fear_web.sql
+++ fr_fear_web.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 27, 2024 at 11:01 PM
+-- Generation Time: Apr 27, 2024 at 11:09 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
@@ -37,6 +37,19 @@ CREATE TABLE `authentication` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `reset_password`
+--
+
+CREATE TABLE `reset_password` (
+  `id` int(11) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `code` varchar(255) NOT NULL,
+  `time` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
@@ -47,7 +60,8 @@ CREATE TABLE `users` (
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `statue` tinyint(4) NOT NULL,
-  `privilege` int(11) NOT NULL
+  `privilege` int(11) NOT NULL,
+  `auth` tinyblob NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -58,6 +72,12 @@ CREATE TABLE `users` (
 -- Indexes for table `authentication`
 --
 ALTER TABLE `authentication`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `reset_password`
+--
+ALTER TABLE `reset_password`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -74,6 +94,12 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `authentication`
 --
 ALTER TABLE `authentication`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `reset_password`
+--
+ALTER TABLE `reset_password`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
