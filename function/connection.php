9a23ff4f525e4ee39601b0e3ca6110abcc9afdde
mahmoudyousef <mahmoudyousef59@outlook.com>
2024-05-08 01:19:47 +0200

    update database

--- function/connection.php
+++ function/connection.php
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 <?php 
     
     $HOST = 'localhost';
@@ -14,21 +14,5 @@
 
     mysqli_set_charset($conn, 'utf8');
 
-=======
-<?php 
-    
-    $HOST = 'localhost';
-    $USER = 'freedom_1_phpma';
-    $PASSWORD = 'DXSqiW!qO0Mr?d78';
-    $DBNAME = 'freedom_1_database';
 
-    $conn =  mysqli_connect($HOST, $USER, $PASSWORD, $DBNAME);
-
-    if(!$conn){
-        die('Connection failed: '.mysqli_connect_error());
-    }
-
-    mysqli_set_charset($conn, 'utf8');
-
->>>>>>> cb2837cd138e21668014c4179f847682d5b0afe5
 ?>
--- function/login.php
+++ function/login.php
@@ -1,5 +1,5 @@
 <?php 
-    require_once 'DB.php';
+    require_once('DB.php');
     $db = new DB();
      // user login or admin login
 
