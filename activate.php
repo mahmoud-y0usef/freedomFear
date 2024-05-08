62c3ea459861d7338df25bc414536629ae079817
mahmoudyousef <mahmoudyousef59@outlook.com>
2024-05-08 04:05:30 +0200

    update activate.php

--- activate.php
+++ activate.php
@@ -4,17 +4,18 @@ include 'inc/header.php';
 ?>
 <div class="welcome-page">
     <div class="uk-width-expand@m page-first-screen">
-        <?php 
+       
+        <div class="fl-hd-cover"></div>
+        <div class="form-login
+            ">
+            <div class="form-login__box">
+            <?php 
             if(isset($_GET['error'])){
                 echo '<div class="uk-alert-danger" uk-alert><a class="uk-alert-close" uk-close></a><p>'.$_GET['error'].'</p></div>';
             }elseif(isset($_GET['success'])){
                 echo '<div class="uk-alert-success" uk-alert><a class="uk-alert-close" uk-close></a><p>'.$_GET['success'].'</p></div>';
             }
         ?>
-        <div class="fl-hd-cover"></div>
-        <div class="form-login
-            ">
-            <div class="form-login__box">
                 <div class="uk-heading
                     -line uk-text-center"><span>Activate Account</span></div>
                     <br>
