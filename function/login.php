87cbae9a8129d4997881ecce654afb5cb7b6721f
mahmoudyousef <mahmoudyousef59@outlook.com>
2024-05-10 04:20:09 +0200

    update login system

--- function/login.php
+++ function/login.php
@@ -3,36 +3,40 @@
 require_once ('DB.php');
 $db = new DB();
 
-if(isset($_POST['login'])){
+if (isset($_POST['login'])) {
     $username_or_email = $_POST['username_or_email'];
     $password = $_POST['password'];
-    if(empty($username_or_email) || empty($password)){
+    if (empty($username_or_email) || empty($password)) {
         header('Location: ../index.php?error=Please fill out the data');
         exit;
-    }else{
+    } else {
         $username_or_email = filter_var($username_or_email, FILTER_SANITIZE_STRING);
         $password = filter_var($password, FILTER_SANITIZE_STRING);
         $user = $db->login($username_or_email, $password, 'account');
         $admin = $db->login($username_or_email, $password, 'admins');
-        if($user){
-            if($user['activate'] == 1){
+        if ($user) {
+            if ($user['activate'] == 1) {
                 session_start();
                 $_SESSION['user'] = $user;
                 header('Location: ../index.php');
                 exit;
-            }else{
+            } else {
                 header('Location: ../index.php?error=Account not activated');
                 exit;
             }
-        }else if($admin){
+        } else if ($admin) {
             session_start();
             $_SESSION['user'] = $admin;
             header('Location: ../admin/index.php');
             exit;
-        }else{
-            echo "invalid pasword";
-            // header('Location: ../index.php?error=Invalid username or password');
-            // exit;
+        } else {
+            error_reporting(E_ALL | E_WARNING | E_NOTICE);
+            ini_set('display_errors', TRUE);
+
+
+            flush();
+            header("Location: ../index.php?error=Invalid username or password");
+            die('should have redirected by now');
         }
     }
 }
