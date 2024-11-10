e3a2c5cd9e982aafc8ae9d14f929e933e69a2f63
mahmoudYousef <mahmoudyousef59@outlook.com>
2024-11-10 21:12:47 +0200

    update php mailer

--- function/DB.php
+++ function/DB.php
@@ -3,9 +3,10 @@ ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 require_once 'connection.php';
-require_once '../Php/vendor/autoload.php';
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
+require_once('../Php/phpmailer/PHPMailer.php');
+require_once('../Php/phpmailer/SMTP.php');
+require_once('../Php/phpmailer/POP3.php');
+require_once('../Php/phpmailer/Exception.php');
 
 class DB
 {
