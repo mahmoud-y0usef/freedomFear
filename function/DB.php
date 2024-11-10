6cfcc23dacc0b36d78c0dc29a26d46aa43ecfe64
mahmoudYousef <mahmoudyousef59@outlook.com>
2024-11-10 21:03:20 +0200

    display error

--- function/DB.php
+++ function/DB.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
 require_once 'connection.php';
 require '../Php/vendor/autoload.php';
 use PHPMailer\PHPMailer\PHPMailer;
