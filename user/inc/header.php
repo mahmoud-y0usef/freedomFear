a581d646c53b6448bfb53405d96fcd0d44c1904a
mahmoudYousef <mahmoudyousef59@outlook.com>
2024-11-17 19:09:37 +0200

    fix

--- user/inc/header.php
+++ user/inc/header.php
@@ -13,8 +13,8 @@ $db = new DB();
 <head>
     <meta charset="utf-8">
     <title>Freedom Fear | <?php echo $title ?></title>
-    <meta content="Templines" name="author">
-    <meta content="TeamHost" name="description">
+    <meta content="Free dom fear" name="author">
+    <meta content="Freedom Fear" name="description">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="HandheldFriendly" content="true">
     <meta name="format-detection" content="telephone=no">
